74e9eee742a23553553a0853dc76df27af1bc82c
NekuSoul <Git@NekuSoul.de>
2017-08-06 14:44:46 +0200

    Initial release

---
+++ DTN-Wallpaper/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Timer = System.Timers.Timer;
+
+namespace DTN_Wallpaper
+{
+    internal static class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        private static float _fileCount;
+
+        [STAThread]
+        private static void Main()
+        {
+            _fileCount = Directory.GetFiles(@"Rotation", @"*.png").Length;
+
+            Timer_Elapsed(null, null);
+            var timer = new Timer { Interval = 24 * 60 / _fileCount * 60 * 1000 * 0.9 };
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            Application.Run();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            int currentTime = (int)((DateTime.Now.Hour * 60 + DateTime.Now.Minute) / (24 * 60 / _fileCount)) + 1;
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (key != null)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(20, 0, Path.Combine(Directory.GetCurrentDirectory(), @"Rotation", currentTime + ".png"), 0x01 | 0x02);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-DTN-Wallpaper
+# DTN-Wallpaper
+
+## Description
+
+This is a tool that runs silently in the background and changes your wallpaper based on the time of day.
+
+## Usage
+
+Copy your wallpapers that should be rotated into the 'Rotation' folder. The first wallpaper has to be called '1.png', the second one '2.png' and so on. So if you want to have one wallpaper for each hour of the day you need 24 images, or 144 images if you want your wallpaper to switch every 10 minutes.  
+Each file has to be a .png and make sure there are no other files in the folder.  
+Just start 'DTN-Wallpaper.exe' and your wallpaper will begin to switch automatically. To stop the program use Task Manager.
+
+## License
+MIT-License: See 'LICENSE' file
